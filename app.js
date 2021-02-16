6e8832e1990ff2b112adb6e6968da455bcc5b42b
Robert Alvarado <treborlp@gmail.com>
2021-02-15 23:12:31 -0500

    Servir un sitio web completo

--- app.js
+++ app.js
@@ -13,6 +13,15 @@ app.get('/hola-mundo', (req, res) => {
     res.send("Bienvenido a Hola mundo");
 });
 
+app.get('/generic', (req, res) => {
+    res.sendFile(__dirname + '/public/generic.html');
+});
+
+app.get('/elements', (req, res) => {
+    res.sendFile(__dirname + '/public/elements.html');
+});
+
+
 app.get('*', (req, res) => {
     res.sendFile(__dirname + '/public/404.html');
 });
