0ae6e8601e8164c28dcb4ae0b4e7c4bb4e886da3
Robert Alvarado <treborlp@gmail.com>
2021-02-15 22:41:14 -0500

    Introduccion a express

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
-const http = require("http")
+const express = require("express")
+const app = express();
+const port = 8080
 
-http.createServer((require, response) => {
-    response.write("Dont make me cry");
-    response.end();
-}).listen(8080);
+app.get('/', (req, res) => {
+    res.send('Home page');
+});
 
-console.log("Escuchando en el puero:", 8080);
+app.get('/hola-mundo', (req, res) => {
+    res.send("Bienvenido a Hola mundo");
+});
+
+app.get('*', (req, res) => {
+    res.send('La pagina solicitada no fue encontrada');
+});
+
+app.listen(port, () => {
+    console.log(`El servidor de ejemplo se encuentra corriendo en el puerto ${port}`);
+})
